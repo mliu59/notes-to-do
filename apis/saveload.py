eb810562b62e4f60925abb795bdf6fbd030af880
mliu59 <mliu59@jhu.edu>
2022-01-24 11:49:32 -0500

    working version

--- apis/saveload.py
+++ apis/saveload.py
@@ -1,15 +1,21 @@
 from flask import request, abort, Blueprint, jsonify
 import json
 import ast
+from datetime import datetime
+import os
+import glob
 
-savefilename = "notes-todo.txt"
+filedir = "C:\\todo\\"
+fileprefix = "notes-todo-"
 
 saveload_api = Blueprint("saveload", __name__, url_prefix="/api/saveload")
 
 @saveload_api.post('/save/')
 def save_state():
-    #print(str(request.json))
-    file1 = open(savefilename,"w")
+    
+    fullfilename = os.path.join(filedir, fileprefix + datetime.now().strftime("%Y-%m-%d-%H-%M")+".txt")
+
+    file1 = open(fullfilename,"w")
     file1.write(str(request.json))
     file1.close()
 
@@ -18,9 +24,12 @@ def save_state():
 
 @saveload_api.get('/load/')
 def load_state():
-    file1 = open(savefilename,"r")
+
+    list_of_files = glob.glob(filedir + "*.txt") # * means all if need specific format then *.csv
+    latest_file = max(list_of_files, key=os.path.getctime)
+
+    file1 = open(latest_file,"r")
     saved_string = file1.readline()
     JSONdata = ast.literal_eval(saved_string)
-    #print(JSONdata['items'])
     
     return jsonify(JSONdata) 
--- static/js/main.js
+++ static/js/main.js
@@ -5,32 +5,44 @@ Vue.component('item', {
     },
     template:  `<div class="row" v-if="item.display">
 
+                    <div class="col col-sm-auto idcol"><b>{{item.arrindex}}</b></div>
+
                     <div class="col col-sm-auto container" v-if="item.depth > 1" >
                         <div class="row">
                             <div v-for="index in (item.depth-1)" class="child-left-padding col-sm-auto"></div>
                         </div>
                     </div>
 
-                    <div v-bind:class="item.showIndentArrow ? 'col-sm-auto visible' : 'col-sm-auto invisible'" v-if="item.displayChildren">
-                    <button class="btn btn-sm" v-on:click="toggleDisplayChildren(item.item_id)">
-                        <i v-bind:class="item.displayChildren ? 'bi bi-caret-down-fill' : 'bi bi-caret-right-fill'"  width="10"></i>
-                    </button>
+                    <div v-bind:class="item.showIndentArrow ? 'col-sm-auto visible' : 'col-sm-auto invisible'">
+                        <button class="btn btn-sm" v-on:click="toggleDisplayChildren(item.item_id)">
+                            <i v-bind:class="item.displayChildren ? 'bi bi-caret-down-fill' : 'bi bi-caret-right-fill'"  width="10"></i>
+                        </button>
                     </div>
 
 
                     <div v-if="item.completed" class="col">
                         <strike><div v-html="item.mainText"></div></strike>
                     </div>
+
                     <div class="col" v-else>
                         <div v-html="item.mainText"></div>
                     </div>
 
-                    <div class="col-sm-auto">{{item.item_id}}</div>
-                    <div class="col-sm-auto" v-if="item.type==='TODO'">
+                    <div class="col-sm-auto" v-bind:class="item.type==='TODO' ? 'visible' : 'invisible'"><span v-bind:class="item.completed ? 'completedDate' : 'notCompletedDate'">{{item.dueDate}}</span></div>
+                    
+                    <div class="col-sm-auto" v-bind:class="item.type==='TODO' ? 'visible' : 'invisible'">
                         <button class="btn btn-sm btn-outline-secondary" v-on:click="toggleComplete(item.item_id)">
                             <i class="bi bi-check-lg" v-bind:class="item.completed ? 'visible' : 'invisible'"  width="10"></i>
                         </button>
                     </div>
+                    
+                    <div class="col-sm-auto btn-group" role="group" aria-label="moveitem">
+                        <button class="btn btn-sm btn-outline-secondary arrowButtons" v-on:click="moveItem(item.item_id, 'left')"><i class="bi bi-arrow-left-short" width="10"></i></button>  
+                        <button class="btn btn-sm btn-outline-secondary arrowButtons" v-on:click="moveItem(item.item_id, 'up')"><i class="bi bi-arrow-up-short" width="10"></i></button>  
+                        <button class="btn btn-sm btn-outline-secondary arrowButtons" v-on:click="moveItem(item.item_id, 'down')"><i class="bi bi-arrow-down-short" width="10"></i></button>  
+                        <button class="btn btn-sm btn-outline-secondary arrowButtons" v-on:click="moveItem(item.item_id, 'right')"><i class="bi bi-arrow-right-short" width="10"></i></button>    
+                    </div>
+                    
                     <div class="col-sm-auto">
                         <button class="btn btn-sm btn-outline-secondary" v-on:click="deleteThisItem(item.item_id)"><i class="bi bi-trash" width="10"></i></button>    
                     </div>
@@ -46,41 +58,132 @@ Vue.component('item', {
 
         toggleComplete : function (id) {
             app.toggleComplete(id)
-        }
+        },
+
+        getIndex: function (id) {
+            return app.findIndex(id)
+        },
+
+        moveItem : function (id, dir) {
+            app.moveItem(id, dir)
+        },
     }
-})
+});
+
+
+
 
 var app = new Vue({
     delimiters: ['[[', ']]'],
     el: '#vue-app',
     data: {
-        items:[],
+        items:{
+            Base : [],
+        },
         totalItems:0,
         lastID:0,
         debug:true,
+        pellContent:"",
+        selectedFolder:"Base",
     },
 
-    watch: {
-        items: function () {
-
-            this.items.forEach(item => {
-                if (this.getDescendants(item.item_id).length > 0) {
-                    item.showIndentArrow = true;
-                }
-            });
-
+    mounted : function (){
+        window.setInterval(() => {
             this.saveState()
-        },
+          }, 60000)
     },
 
     methods : {
+        deleteFolder: function () {
+            
+            console.log(Object.keys(this.items))
+
+            if (Object.keys(this.items).length > 1) {
+                alert("Are you sure?")
+                delete this.items[this.selectedFolder]
+                this.selectedFolder = Object.keys(this.items)[0]
+            } else {
+                console.log("Cant delete only folder")
+            }
+
+            this.refresh()
+            
+        },
+
+        submitNewFolderName: function() {
+            const input = document.getElementById("edit-folder-input").value
+            console.log(input)
+            this.items[input] = this.items[this.selectedFolder]
+            delete this.items[this.selectedFolder]
+            this.selectedFolder = input
+
+            this.refresh()
+        },
+
+        editFolderName: function () {
+            let editfield = document.getElementById("edit-folder-name")
+            editfield.classList.remove("invisible")
+            editfield.classList.add("visible")
+
+        },
+
+        addFolder: function () {
+            let basestring = "Base"
+            while (this.items[basestring]) {
+                basestring += "-new"
+            }
+
+            this.items[basestring] = []
+            this.refresh()
+        },
+
+        changeActiveFolder: function (key) {
+            if (this.items[key]) {
+                this.selectedFolder = key
+            }
+            this.refresh()
+        },
+
+        refresh: function () {
+            //console.log(Object.keys(this.items))
+            Object.keys(this.items).forEach(key => {
+                if (this.items[key].length > 0 && key === this.selectedFolder) {
+                    this.items[key].forEach((item, index) => {
+                        //console.log(item)
+                        if (this.getDescendants(item.item_id).length > 0) {
+                            item.showIndentArrow = true;
+                        } else {
+                            item.showIndentArrow = false;
+                        }
+        
+                        item.arrindex = index;
+                    });
+                }
+            });
+
+            this.$forceUpdate
+        },
+
         resetData: function() {
-            this.items=[]
+            this.items={
+                Base: []
+            }
             this.totalItems=0
             this.lastID=0
             this.debug=true
+            this.pellContent=""
+            
+            //this.resetPell()
+            
         },
 
+        getAll: function (field) {
+            let output = []
+            this.items[this.selectedFolder].forEach(item => {
+                output.push(item[field])
+            });
+            return output
+        },
 
         toggleDisplayChildren: function (id) {
 
@@ -94,23 +197,30 @@ var app = new Vue({
         },
 
         toggleComplete: function (id) {
-            this.setVal(id, "completed", !this.getVal(id, "completed"))
+
+            const toggled = !this.getVal(id, "completed")
+            this.setVal(id, "completed", toggled)
+
+            const desc = this.getDescendants(id)
+            desc.forEach(item => {
+                this.setVal(item, "completed", toggled)
+            });
         },
 
         getVal: function (id, field) {
 
             const index = this.findIndex(id)
-            return this.items[index][field]
+            return this.items[this.selectedFolder][index][field]
         },
 
         setVal: function (id, field, val) {
 
             const index = this.findIndex(id)
-            this.items[index][field] = val
+            this.items[this.selectedFolder][index][field] = val
         },
 
         findIndex: function (id) {
-            const index = this.items.findIndex(object => {
+            const index = this.items[this.selectedFolder].findIndex(object => {
                 return object.item_id === id;
             });
 
@@ -120,11 +230,11 @@ var app = new Vue({
         getDescendants: function (id) {
             let output = [];
             const index = this.findIndex(id)
-            const startingDepth = this.items[index].depth
+            const startingDepth = this.items[this.selectedFolder][index].depth
 
-            for (let i = index + 1; i < this.items.length; i++) {
-                if (this.items[i].depth > startingDepth) {
-                    output.push(this.items[i].item_id)
+            for (let i = index + 1; i < this.items[this.selectedFolder].length; i++) {
+                if (this.items[this.selectedFolder][i].depth > startingDepth) {
+                    output.push(this.items[this.selectedFolder][i].item_id)
                 } else {
                     break;
                 }
@@ -134,16 +244,18 @@ var app = new Vue({
         },
 
         displayStatus: function () {
-            console.log(this.getDescendants(this.items[0].item_id))
+            console.log(this.$data)
         },
 
-        addItem: function (item) {
+        addItem: function (item, index) {
 
-            item.item_id = this.lastID
-            this.items.push(item)
+            this.items[this.selectedFolder].splice(index, 0, item)
+            //this.items.push(item)
 
             this.totalItems++;
             this.lastID++;
+
+            this.refresh();
         },
 
 
@@ -151,16 +263,65 @@ var app = new Vue({
             
             const index = this.findIndex(id)
             
-            let starting_depth = this.items[index].depth
+            let starting_depth = this.items[this.selectedFolder][index].depth
 
             if (index > -1) {
                 do {
-                    this.items.splice(index, 1)
+                    this.items[this.selectedFolder].splice(index, 1)
                     this.totalItems--;
                     
-                } while (this.items[index] && this.items[index].depth > starting_depth)
+                } while (this.items[this.selectedFolder][index] && this.items[this.selectedFolder][index].depth > starting_depth)
             }
             
+        },
+
+        moveItem: function (id, dir) {
+            const index = this.findIndex(id)
+            const depth = this.getVal(id, 'depth')
+
+            switch (dir) {
+                case "left":
+                    if (depth > 1) {
+                        this.items[this.selectedFolder][index].depth--;
+                    }
+                    break;
+                case "right":
+                    if (index > 0 && (this.items[this.selectedFolder][index - 1].depth === depth || this.items[this.selectedFolder][index - 1].depth-1 === depth)) {
+                        this.items[this.selectedFolder][index].depth++;
+                    } 
+                    break;
+                case "up":
+                    if (index > 0) {
+                        if (this.items[this.selectedFolder][index - 1].depth === depth) {
+                            this.swap(index, index-1)
+                        } else if (this.items[this.selectedFolder][index - 1].depth === depth - 1) {
+                            this.items[this.selectedFolder][index].depth--;
+                            this.swap(index, index-1)
+                        }
+                    }
+                    break;
+                case "down":
+                    if (index < this.totalItems-1) {
+                        if (this.items[this.selectedFolder][index + 1].depth === depth) {
+                            this.swap(index, index+1)
+                        } else if (this.items[this.selectedFolder][index + 1].depth === depth - 1) {
+                            this.items[this.selectedFolder][index+1].depth--;
+                            this.swap(index, index+1)
+                        }
+                    }
+                    break;
+                default:
+                    console.log(err)
+            }
+
+
+            this.refresh();
+        },
+
+        swap: function (a, b) {
+            var temp = this.items[this.selectedFolder][a]
+            this.items[this.selectedFolder][a] = this.items[this.selectedFolder][b]
+            this.items[this.selectedFolder][b] = temp;
         },
 
         saveState: async function () {
@@ -189,38 +350,42 @@ var app = new Vue({
                 for (let item in body) {
                     this[item] = body[item]
                 }
-                //console.log(body)
             } catch(e) {
                 console.log(e)
             }
         },
-    },
 
-    created : function () {
-        this.resetData();
-        //this.loadState();
-    }
+        resetPell: function () {
+            var editor = document.getElementsByClassName("pell-content")[0];
+            editor.innerHTML = "";
+            this.pellContent = "";
+        },
 
-})
-
-
-var input_app = new Vue({
-    delimiters: ['[[', ']]'],
-    el: '#input-app',
-    data: {
-        pellContent:""
-
-    },
-    methods : {
         submitNewItem: function () {
-            let item = this.defaultItem();
 
-            item.mainText = this.pellContent
-            item.type = document.getElementById("typeSelect").value
+            if (this.pellContent === "" || this.pellContent === "<div></div>") {
+                console.log("cannot submit empty")
+            } else {
 
-            item.dueDate = document.getElementById("dueDateSelect").value
+                let item = this.defaultItem();
 
-            app.addItem(item)
+                item.mainText = this.pellContent
+                item.type = document.getElementById("typeSelect").value
+                item.dueDate = document.getElementById("dueDateSelect").value
+
+                const rel = document.getElementById("relationToEl").value
+                
+                const idstring = document.getElementById("elSelected").value
+                
+                const posindex = parseInt(idstring.substring(0, idstring.indexOf(':')))
+
+                const index = this.getIndexOfRelPos(rel, posindex)
+                
+                item.depth = index['depth']
+                this.addItem(item, index['index'])
+
+                this.resetPell()
+            }
         },
 
         defaultItem: function () {
@@ -232,12 +397,92 @@ var input_app = new Vue({
             item.display = true;
             item.displayChildren = true;
             item.completed = false;
+            item.arrindex = 0;
             
             return item
         },
 
+        getAllActiveItemNames: function () {
+
+            let ids = this.getAll("arrindex")
+            let names = this.getAll("mainText")
+
+            let output = []
+
+            for (let i = 0; i < this.items[this.selectedFolder].length; i++) {
+                output.push(String(ids[i]) + ": " + String(names[i]))
+            }
+
+            return output
+        },
+
+        getIndexOfNextSibling: function (id) {
+
+            let sib_index = this.findIndex(id)
+            let sib_depth = this.getVal(id, "depth")
+
+            for (let i = sib_index + 1; i < this.items[this.selectedFolder].length; i ++) {
+                //console.log(String(this.items[i].depth) + " " + String(sib_depth))
+                if (this.items[this.selectedFolder][i].depth <= sib_depth) {
+                    //console.log("found next sibling at " + String(i))
+                    return i
+                }
+            }
+
+            console.log("cannot find next sibling")
+            return -1
+
+        },
+
+        getIndexOfRelPos: function (relation, posindex) {
+
+
+            let id = posindex > -1 ? this.items[this.selectedFolder][posindex].item_id : "ROOT"
+
+            let output = {
+                index: this.items[this.selectedFolder].length,
+                depth: 1
+            }
+
+            if (id === "ROOT"){ 
+                if (relation === "FIRST-CHILD") {
+                    output.index = 0
+                }
+            } else {
+                switch (relation) {
+                    case 'FIRST-CHILD':
+                        output.depth = this.getVal(id, 'depth') + 1
+                        output.index = this.findIndex(id) + 1
+                        break;
+                    case 'LAST-CHILD':
+                        output.depth = this.getVal(id, 'depth') + 1
+                        let sib_index = this.getIndexOfNextSibling(id)
+                        output.index = sib_index === -1 ? this.items[this.selectedFolder].length : sib_index
+                        break;
+                    case 'PREV-SIBLING':
+                        output.depth = this.getVal(id, 'depth')
+                        output.index = this.findIndex(id)
+                        break;
+                    case 'NEXT-SIBLING':
+                        output.depth = this.getVal(id, 'depth')
+                        let s_index = this.getIndexOfNextSibling(id)
+                        output.index = s_index === -1 ? this.items[this.selectedFolder].length : s_index
+                        break;
+                }
+            }
+
+            return output
+        }
+    },
+
+    created : function () {
+        //this.resetData();
+        this.loadState();
+        this.refresh();
     }
+
 });
+
 
 const pell = window.pell;
 const editor = document.getElementById("editor");
@@ -245,9 +490,10 @@ const editor = document.getElementById("editor");
 pell.init({
   element: editor,
   onChange: (html) => {
-    input_app.$data.pellContent = html;
+    app.$data.pellContent = html;
   }
 })
+
 
 function getCurDate() {
     return new Date().toISOString().split('T')[0]
